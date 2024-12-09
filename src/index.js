29eac0c84eac93ebfce642359c0e3a4f6f31461e
paulohwebdev <nemeth.paulo@outlook.com>
2024-12-09 17:43:47 -0300

    juntando o cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import livrosRoutes from "./routes/livrosRoutes.js";
 import authenticate from "./database/connection.js";
 import verificarAdm from "./middlewares/adminMiddleware.js";
@@ -7,6 +8,8 @@ import verificarAdm from "./middlewares/adminMiddleware.js";
 authenticate();
 
 const servidor = express();
+
+servidor.use(cors({ origin: "*" }));
 
 servidor.use(express.json());
 
